521eef92fc3071de61aba2c1d2b4a7eb3b78b942
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-27 11:11:42 +0300

    Introduce me swiftUI Design

--- testsh/ContentView.swift
+++ testsh/ContentView.swift
@@ -9,15 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        VStack(spacing: 100){
+            Image("Image")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color("color")))
+            
+          
+            
+        
+            HStack{
+                Text("\"I am a set of letters, a combination of words, and the content of a story.\"").font(.system(size: 24, design: .rounded)).foregroundColor(.black)
+                
+            }
+            Spacer() }
+        
+            .padding()
+        .background(Color("Color"))}
+        
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
